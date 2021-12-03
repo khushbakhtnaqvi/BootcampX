90f1161d0fb4d620937e74dc87f2de2ff741f763
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-12-03 04:01:34 +0000

    sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,36 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-ORDER BY teacher;`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
-  })
-}).catch(err => console.error('query error', err.stack));
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+LIMIT $2;`
+
+  pool.query(queryString, values);
+
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// ORDER BY teacher;`)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.cohort}: ${user.teacher}`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
 
 // pool.query(`
 // SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
